7a3ff85ef5a3effeec51f93159d4c47a21d072b5
PierProtocolFinance <p2psolana@gmail.com>
2023-12-28 17:27:11 +0100

    metamask connect, disconnect

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Web3 from 'web3';
 import Link from "next/link";
 import logo from "../../assets/images/logo.png";
 import { HiOutlineMenu } from "react-icons/hi";
@@ -7,15 +8,69 @@ import Image from "next/image";
 const Header = () => {
   const [offcanvasOpen, setOffcanvasOpen] = useState(false);
 
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      const web3Instance = new Web3(window.ethereum);
+      setWeb3(web3Instance);
+      checkIfWalletIsConnected();
+      window.ethereum.on('accountsChanged', handleAccountsChanged);
+    } else {
+      console.log("Please install MetaMask!");
+    }
+
+    // Clean up
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
+      }
+    };
+  }, []);
+
+  const handleAccountsChanged = (accounts) => {
+    if (accounts.length === 0) {
+      // Wallet is disconnected or the user has switched accounts
+      setIsConnected(false);
+      setAccounts([]);
+    } else {
+      setAccounts(accounts);
+      setIsConnected(true);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      if (accounts.length > 0) {
+        setAccounts(accounts);
+        setIsConnected(true);
+      }
+    } catch (error) {
+      console.error("Error checking wallet connection", error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setAccounts(accounts);
+      setIsConnected(true);
+    } catch (error) {
+      console.error("Error connecting to MetaMask", error);
+    }
+  };
+
   const toggleOffcanvas = () => {
     setOffcanvasOpen(!offcanvasOpen);
   };
   return (
     <>
       <nav
-        className={`navbar navbar-expand-lg navbar_main ${
-          offcanvasOpen ? "offcanvas-open" : ""
-        }`}
+        className={`navbar navbar-expand-lg navbar_main ${offcanvasOpen ? "offcanvas-open" : ""
+          }`}
       >
         <div className="container">
           <Link className="logo" href="/">
@@ -64,8 +119,11 @@ const Header = () => {
               </ul>
               <div className="nav-btn d-lg-flex align-items-center justify-content-center mt-lg-0 mt-4 ms-auto">
                 <div className="d-flex gap-2 align-items-center justify-content-center mt-4 mt-lg-0">
-                  <Link href="/" className="btn-lg navbar-btn">
-                    <span>Connect Wallet</span>
+                  <Link href={""} className="btn-lg navbar-btn" onClick={connectWallet}>
+                    {isConnected ?
+                      <span>Connected</span> :
+                      <span>Connect Wallet</span>
+                    }
                   </Link>
                 </div>
               </div>
