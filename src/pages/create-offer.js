4089fc578328da89dd016d6ed0d464bf7dd885ae
Konan <ikedahiroshi517@gmail.com>
2024-01-30 04:32:34 +0100

    update dropdown

--- src/pages/create-offer.js
+++ src/pages/create-offer.js
@@ -1,15 +1,32 @@
 import OtherHeader from '@/components/global/OtherHeader'
 import Head from 'next/head'
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { networks } from '@/utils/svg';
 
 export default function CreateOffer() {
     const router = useRouter();
     const [tabStatus, setTabStatus] = useState('network');
+    const [selectedNetwork, setSelectedNetwork] = useState('Solana');
+
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef(null);
+    const networks = ['Ethereum', 'Bsc', 'Polygon', 'Solana']; // Example networks
+
+    const toggleDropdown = () => setIsOpen(!isOpen);
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
 
     useEffect(() => {
-        console.log(tabStatus)
-    }, [])
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
         <>
             <Head>
@@ -80,27 +97,20 @@ export default function CreateOffer() {
                                     <div>
                                         Network
                                     </div>
-                                    <div className='selected-network'>
-                                        <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <circle cx="18" cy="18" r="17.5" fill="black" stroke="black" />
-                                            <g clip-path="url(#clip0_0_1)">
-                                                <path d="M26.9073 22.7879L23.9359 25.9269C23.8713 25.9951 23.7931 26.0494 23.7063 26.0866C23.6194 26.1237 23.5257 26.1429 23.4311 26.1428H9.34496C9.27775 26.1428 9.212 26.1235 9.1558 26.0872C9.09959 26.0509 9.05538 25.9992 9.02859 25.9385C9.0018 25.8778 8.9936 25.8107 9.005 25.7454C9.0164 25.6802 9.04689 25.6196 9.09275 25.5712L12.0664 22.4323C12.1309 22.3643 12.2088 22.31 12.2953 22.2729C12.3819 22.2357 12.4753 22.2165 12.5697 22.2163H26.655C26.7223 22.2163 26.788 22.2357 26.8442 22.272C26.9004 22.3083 26.9446 22.36 26.9715 22.4207C26.9982 22.4814 27.0064 22.5485 26.995 22.6137C26.9836 22.679 26.9531 22.7395 26.9073 22.7879ZM23.9359 16.467C23.8713 16.3988 23.7931 16.3444 23.7063 16.3073C23.6194 16.2701 23.5257 16.251 23.4311 16.251H9.34496C9.27775 16.251 9.212 16.2704 9.1558 16.3067C9.09959 16.343 9.05538 16.3947 9.02859 16.4554C9.0018 16.5161 8.9936 16.5832 9.005 16.6484C9.0164 16.7137 9.04689 16.7742 9.09275 16.8226L12.0664 19.9616C12.1309 20.0296 12.2088 20.0838 12.2953 20.121C12.3819 20.1581 12.4753 20.1774 12.5697 20.1775H26.655C26.7223 20.1775 26.788 20.1582 26.8442 20.1219C26.9004 20.0856 26.9446 20.0339 26.9715 19.9732C26.9982 19.9125 27.0064 19.8454 26.995 19.7801C26.9836 19.7149 26.9531 19.6543 26.9073 19.6059L23.9359 16.467ZM9.34496 14.2122H23.4311C23.5257 14.2123 23.6194 14.1931 23.7063 14.156C23.7931 14.1188 23.8713 14.0645 23.9359 13.9963L26.9073 10.8573C26.9531 10.8089 26.9836 10.7484 26.995 10.6831C27.0064 10.6179 26.9982 10.5508 26.9715 10.4901C26.9446 10.4293 26.9004 10.3777 26.8442 10.3414C26.788 10.305 26.7223 10.2857 26.655 10.2857H12.5697C12.4753 10.2859 12.3819 10.3051 12.2953 10.3423C12.2088 10.3794 12.1309 10.4337 12.0664 10.5017L9.09352 13.6406C9.04771 13.689 9.01722 13.7495 9.0058 13.8146C8.99437 13.8798 9.00251 13.9468 9.0292 14.0075C9.0559 14.0682 9.09999 14.1199 9.15608 14.1563C9.21217 14.1926 9.27782 14.2121 9.34496 14.2122Z" fill="url(#paint0_linear_0_1)" />
-                                            </g>
-                                            <defs>
-                                                <linearGradient id="paint0_linear_0_1" x1="10.5194" y1="26.5208" x2="25.0416" y2="9.90071" gradientUnits="userSpaceOnUse">
-                                                    <stop offset="0.08" stop-color="#9945FF" />
-                                                    <stop offset="0.3" stop-color="#8752F3" />
-                                                    <stop offset="0.5" stop-color="#5497D5" />
-                                                    <stop offset="0.6" stop-color="#43B4CA" />
-                                                    <stop offset="0.72" stop-color="#28E0B9" />
-                                                    <stop offset="0.97" stop-color="#19FB9B" />
-                                                </linearGradient>
-                                                <clipPath id="clip0_0_1">
-                                                    <rect width="18" height="15.8571" fill="white" transform="translate(9 10.2857)" />
-                                                </clipPath>
-                                            </defs>
-                                        </svg>
-                                        <span>Solana</span>
+                                    <div ref={dropdownRef}>
+                                        <div className='selected-network' onClick={toggleDropdown}>
+                                            {networks[selectedNetwork]}
+                                            <span>{selectedNetwork}</span>
+                                        </div>
+                                        {isOpen && (
+                                            <ul>
+                                                {networks.map(network => (
+                                                    <li key={network} onClick={() => { setIsOpen(false); console.log(`Selected ${network}`); }}>
+                                                        {network}
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        )}
                                     </div>
                                     <div className='navigate-button'>
                                         <button className='back' onClick={() => router.push("/dashboard")}>
