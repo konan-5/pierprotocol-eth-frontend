790b5b028d40d5cbd7e3767bf4f7b484eb8ee1f7
konan-5 <ikedahiroshi517@gmail.com>
2024-03-02 16:06:17 +0000

    remove sei

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,19 +3,9 @@ import OtherHeader from '@/components/global/OtherHeader'
 import Dashboard from '@/components/openPositions/Dashboard'
 import Head from 'next/head'
 
-import { useMemo } from 'react';
-
-require('@solana/wallet-adapter-react-ui/styles.css');
-
-import { SeiWalletProvider } from '@sei-js/react';
-
 export default function DashboardPage() {
-    const rpcUrl = 'https://rpc.atlantic-2.seinetwork.io/';
-    const restUrl = 'https://rest.atlantic-2.seinetwork.io/';
-    const chainId = 'atlantic-2';
-
     return (
-        <SeiWalletProvider chainConfiguration={{ chainId, restUrl, rpcUrl }} wallets={['compass', 'fin']}>
+        <>
             <Head>
                 <title> Pier Protocol Dashboard</title>
             </Head>
@@ -23,6 +13,6 @@ export default function DashboardPage() {
                 <OtherHeader />
                 <Dashboard />
             </main>
-        </SeiWalletProvider>
+        </>
     )
 }
